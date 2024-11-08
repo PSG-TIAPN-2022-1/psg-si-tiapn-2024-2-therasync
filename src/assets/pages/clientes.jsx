fcf48ad3fbe0ce79c91143a67d35fc77c3c87530
Grazielle Sorrentino <graziellesorrentino15@gmail.com>
2024-11-07 22:18:55 -0300

    Adicionando o modal de clientes no branch Backend

--- src/assets/pages/clientes.jsx
+++ src/assets/pages/clientes.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import '../styles/clientes.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown,Button,Modal } from 'react-bootstrap';
+import ModalCliente from '../components/ModalCliente';
+
 
 
 const Clientes = () => {
+
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
     /*Tudo correspondente a pagina deve estar dentro de container para melhor renderização do conteúdo da pagina */
-    <div className='container'>
-      <p id='titulo_container'>Clientes</p>
+    <div className='clientes_container'>
+      <p id='clientes_titulo_container'>Clientes</p>
 
+      <div className='options_container'>
+        <Dropdown>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            Selecionar Opção
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Opção 1</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Opção 2</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Opção 3</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
 
       <div className="tabela_clientes">
       <table>
@@ -18,7 +38,7 @@ const Clientes = () => {
             <th>Idade</th>
             <th>Última Consulta</th>
             <th>Próxima Consulta</th>
-            <th>Detalhes</th>
+            <th>sdf</th>
           </tr>
         </thead>
         <tbody>
@@ -28,23 +48,19 @@ const Clientes = () => {
             <td>35</td>
             <td>10/10/2024</td>
             <td>10/12/2024</td>
-            <td><button class="btn-more">Mais</button></td>
-          </tr>
-          <tr>
-            <td>Inativo</td>
-            <td>João Santos</td>
-            <td>40</td>
-            <td>20/08/2024</td>
-            <td>—</td>
-            <td><button class="btn-more">Mais</button></td>
-          </tr>
-          <tr>
-            <td>Ativo</td>
-            <td>Ana Costa</td>
-            <td>28</td>
-            <td>15/09/2024</td>
-            <td>15/11/2024</td>
-            <td><button class="btn-more">Mais</button></td>
+            <td>
+              <>
+                <Button variant="primary" onClick={() => setModalShow(true)}>
+                  Mais
+                </Button>
+
+                <ModalCliente         
+                show={modalShow}
+                onHide={() => setModalShow(false)}>
+
+                </ModalCliente>
+              </>
+            </td>
           </tr>
         </tbody>
       </table>
