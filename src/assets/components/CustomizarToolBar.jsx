6d6d8e120ba971ad373cf353bae2b59b28221c87
Grazielle Sorrentino <graziellesorrentino15@gmail.com>
2024-12-09 09:50:46 -0300

    Botões

--- src/assets/components/CustomizarToolBar.jsx
+++ src/assets/components/CustomizarToolBar.jsx
@@ -3,27 +3,30 @@ import { Views } from 'react-big-calendar';
 
 function CustomizarToolBar({ label, onNavigate, onView, view }) {
   return (
-    <div className="custom-toolbar">
+    <div className="custom-toolbar" style={{ margin: "0 auto", padding: "20px"}}>
       <div className="navigation-buttons">
-        <button onClick={() => onNavigate('PREV')}>Anterior</button>
+        <button onClick={() => onNavigate('PREV')} style={{ width: "100px" ,backgroundColor: "red"}}>Anterior</button>
         <span className="navigation-buttons-label">{label}</span>
-        <button onClick={() => onNavigate('NEXT')}>Próximo</button>
+        <button onClick={() => onNavigate('NEXT')} style={{ width: "100px" , backgroundColor: "green"}}>Próximo</button>
       </div>
 
       <div className="view-buttons">
         <button
+          style={{ width: "100px" }}
           onClick={() => onView(Views.MONTH)}
           className={view === Views.MONTH ? 'active' : ''}
         >
           Mês
         </button>
         <button
+          style={{ width: "100px" }}
           onClick={() => onView(Views.WEEK)}
           className={view === Views.WEEK ? 'active' : ''}
         >
           Semana
         </button>
         <button
+          style={{ width: "100px" }}
           onClick={() => onView(Views.DAY)}
           className={view === Views.DAY ? 'active' : ''}
         >
